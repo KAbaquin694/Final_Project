e430771ac56992db832274e1a8bcf73008ee9015
Khalel <KAbaquin@west-mec.org>
2019-12-10 15:21:28 -0700

    Update 2
    installed Node.js, attempting to get the Spotify API up & running

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 var port = 7777;
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+// var http = require("http");
+
+// http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type":
+//     "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }).listen(7777);
 
 const hbs = require('express-handlebars')({
     extname: '.hbs',
@@ -14,22 +22,24 @@ app.get('/', (req, res) => {
     res.send("This Is Text.")
 });
 
-app.get('/:id', async (req, res) => {
-try {
-    const URI = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`;
-    const pokemonData = await fetch(URI);
-    const json = await pokemonData.json();
-    pokeName = await json.name;
-    pokeImg = await json.sprites.front_default;
+// GET https://icanhazdadjoke.com/
+
+// app.get('/:id', async (req, res) => {
+// try {
+//     const URI = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`;
+//     const pokemonData = await fetch(URI);
+//     const json = await pokemonData.json();
+//     pokeName = await json.name;
+//     pokeImg = await json.sprites.front_default;
     
-    await res.render('index', {
-        name: pokeName,
-        img: pokeImg
-    });
-} catch (error) {
-    console.log(error);
-}
-});
+//     await res.render('index', {
+//         name: pokeName,
+//         img: pokeImg
+//     });
+// } catch (error) {
+//     console.log(error);
+// }
+// });
 
 app.listen(process.env.PORT || port, () => {
     console.log(`Server started on port ${port}`);
